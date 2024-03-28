243f67f1e54a29461aaf90caf19e6d5d8d4f9617
RyBry <rion257@outlook.com>
2024-03-28 18:23:50 -0400

    attendance 3/28/2024 implemented color picker

--- components/Button.js
+++ components/Button.js
@@ -3,8 +3,9 @@ import styles from "./Button.module.scss";
 /* Every component in react is a function */
 /* We pass deconstructed properties AKA "props" {aSingleProp} 
     into component declarations */
-const Button = ({ label, clickHandler }) => {
+const Button = ({ backgroundColor, clickHandler, label }) => {
     return <button
+        style={{ backgroundColor: backgroundColor }}
         className={styles.btn}
         onClick={clickHandler}
     >{label}</button>
--- components/ColorPicker.js
+++ components/ColorPicker.js
@@ -1,16 +1,59 @@
+import { useState } from "react";
+import Button from "./Button";
+
+import styles from "./ColorPicker.module.scss";
+
 const colors = [
     {
         name: "red",
         value: "#ff0000",
     },
+    {
+        name: "green",
+        value: "#00FF00",
+    },
+    {
+        name: "blue",
+        value: "#0000FF",
+    },
+    {
+        name: "yellow",
+        value: "#FFFF00",
+    },
+    {
+        name: "teal",
+        value: "#008080",
+    },
+    {
+        name: "orange",
+        value: "#FFa500",
+    },
 ];
 
 const ColorPicker = () => {
-    return <div
-        /* First curly bracket allows us to put code into style. Second bracket treats input as an object */
-        style={{
-            backgroundColor: colors[0].value,
-        }}>Color picker</div>;
+    /* [State value, access function to update state ] = useState(state starting value) */
+    const [activeIndex, setActiveIndex] = useState(1);
+    return (
+        <div className={styles.colorpicker}>
+            <div className={styles.colorpicker_block}
+                /* First curly bracket allows us to put code into style. Second bracket treats input as an object */
+                style={{
+                    backgroundColor: colors[activeIndex].value,
+                }}>
+                <h2 className={styles.colorpicker_name}>{colors[activeIndex].name}</h2>
+            </div>
+            {/* map loops through every element, giving element and index. */}
+            {colors.map((color, index) => {
+                return <Button
+                    key={index}
+                    backgroundColor={color.value}
+                    label={color.name}
+                    clickHandler={() => {
+                        setActiveIndex(index);
+                    }} />
+            })}
+        </div >
+    );
 };
 
 export default ColorPicker;
