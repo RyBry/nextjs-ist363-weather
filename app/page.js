86b822b5ab6b986a644d5808a8a3e24c81283dce
RyBry <rion257@outlook.com>
2024-04-07 10:12:31 -0400

    environment variable added and browser geolocation

--- app/page.js
+++ app/page.js
@@ -6,28 +6,54 @@ import ButtonDemo from '../components/ButtonDemo';
 import ColorPicker from "../components/ColorPicker";
 import PeoplePicker from "../components/PeoplePicker"
 
-import { getPeople, getWeatherData } from "../lib/api";
+import { getPeople, getWeatherData, getGeoLocation, getWeatherDataByLatLon } from "../lib/api";
 
 import Image from 'next/image';
 
 const Homepage = () => {
   const [weatherData, setWeatherData] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
   const peopleArr = getPeople();
 
-  /* For useEffect, supply a function, declare an asynchronous function within, and execute that function, which updates weatherData. Set the side-effect array to weatherData */
+  /* If null element is provided to dependency list, then effect triggers automatically on startup */
+  useEffect(() => {
+    getGeoLocation()
+      .then((position) => {
+        console.log(position);
+        setLocation(position);
+      })
+      .catch((error) => {
+        console.log(error);
+        setErrorMessage(error);
+      });
+  }, [])
+
   useEffect(() => {
     const fetchData = async () => {
-      const response = await getWeatherData();
+      const response = await getWeatherDataByLatLon(location);
       setWeatherData(response);
     };
-    fetchData();
-  }, [weatherData]);
+    /* we declare fetch above---but that doesn't mean we run it! Still need to call directly below. */
+    location ? fetchData() : null; /* we have to wait for location to hold something before executing fetch---or errors!. */
+  }, [location]);
+
+  /* For useEffect, supply a function, declare an asynchronous function within, and execute that function, which updates weatherData. Set the side-effect array to weatherData */
+  /*   useEffect(() => {
+      const fetchData = async () => {
+        const response = await getWeatherData();
+        setWeatherData(response);
+      };
+      fetchData();
+    }, [weatherData]); */
 
   return (
     <div>
       <h1>Weather app</h1>
+      {errorMessage && <div>{errorMessage}</div>}
       {/* Wait until weatherData exists before showing div! */}
-      {weatherData ? <div>
+      {weatherData && <div>
         <h2>{weatherData.city.name}</h2>
         <p>Current temp: {weatherData.list[0].main.temp}&deg; F</p>
         <p>{weatherData.list[0].weather[0].description}</p>
@@ -36,7 +62,7 @@ const Homepage = () => {
           alt={`Weather icon`}
           width={100}
           height={100} />
-      </div> : "Loading..."}
+      </div>}
 
       {/*       <PeoplePicker people={peopleArr} />
       <ButtonDemo />
