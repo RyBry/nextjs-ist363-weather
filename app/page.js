7fab546ba48705e228f2eb20d0cfabe6effb6897
RyBry <rion257@outlook.com>
2024-04-09 18:29:01 -0400

    attendance 4/9/24: 5-day forecast

--- app/page.js
+++ app/page.js
@@ -14,6 +14,8 @@ const Homepage = () => {
   const [weatherData, setWeatherData] = useState(null);
   const [location, setLocation] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [daysOfWeek, setDaysOfWeek] = useState(null);
+  const [activeDayIndex, setActiveDayIndex] = useState(0);
 
   const peopleArr = getPeople();
 
@@ -38,6 +40,22 @@ const Homepage = () => {
     /* we declare fetch above---but that doesn't mean we run it! Still need to call directly below. */
     location ? fetchData() : null; /* we have to wait for location to hold something before executing fetch---or errors!. */
   }, [location]);
+
+  useEffect(() => {
+    const tempWeek = [];
+    // filter out days of the week. "block" is the name of our weather import data for each particular loop.
+    weatherData && weatherData.list.filter((block) => {
+      const date = new Date(block.dt * 1000); // multiply by 1000 to get to ms (because javascript interpets in milliseconds)
+      const options = { weekday: "short" }
+      const day = date.toLocaleDateString("en-US", options);
+      //console.log(day);
+      if (!tempWeek.includes(day)) {
+        tempWeek.push(day);
+      }
+    });
+    setDaysOfWeek(tempWeek);
+    // then set state with the das of the week
+  }, [weatherData])
 
   /* For useEffect, supply a function, declare an asynchronous function within, and execute that function, which updates weatherData. Set the side-effect array to weatherData */
   /*   useEffect(() => {
@@ -67,7 +85,24 @@ const Homepage = () => {
       {/*       <PeoplePicker people={peopleArr} />
       <ButtonDemo />
       <ColorPicker /> */}
-    </div >
+      {daysOfWeek && (
+        <section>
+          <ul>
+            {daysOfWeek.map((day, index) => {
+              return <li key={index}>{day}</li>
+            })}
+
+          </ul>
+          <div>{weatherData?.list.filter((block) => {
+            const date = new Date(block.dt * 1000);
+            const options = { weekday: "short" };
+            const day = date.toLocaleTimeString("en-US", options);
+            return day === daysOfWeek[activeDayIndex]; // only return days that match current day
+          }).map((key, index) => {
+            return <p key={index}>{block.main.temp}</p>
+          })}</div>
+        </section>)}
+    </div>
   );
 };
 
